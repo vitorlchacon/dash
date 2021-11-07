0ec2d75816911f195af5b87a7790072d69028506
V. Chacon <vitorlchacon@gmail.com>
2021-11-07 19:38:05 +0000

    Stop script

--- stop.py
+++ stop.py
@@ -1,7 +1,7 @@
 import os
 
-pids = os.popen("ps aux | grep dash.py | awk '{print $2}'")
+pids = os.popen("ps aux | grep dash.py | awk '{print $2}'").readlines()
 if (len(pids) > 0):
-    pid = pids.readlines()[0]
+    pid = pids[0]
     print("Closing dash.py with PID {}", pid)
     os.system('kill '+pid)
