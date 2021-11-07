0a267770e160c6887698bbfa7811d3241598a312
V. Chacon <vitorlchacon@gmail.com>
2021-11-07 20:06:27 +0000

    Changed to flask structure

--- dash.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/home')
-def home():
-    return '<h1>Dashboard</h1>'
-
-app.run()
--- stop.py
+++ stop.py
@@ -1,7 +1,7 @@
 import os
 
-pids = os.popen("ps aux | grep dash.py | awk '{print $2}'").readlines()
+pids = os.popen("ps aux | grep app.py | awk '{print $2}'").readlines()
 if (len(pids) > 0):
     pid = pids[0]
-    print("Closing dash.py with PID {}", pid)
+    print("Closing app.py with PID: ", pid)
     os.system('kill '+pid)
